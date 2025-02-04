78fd234b7d3ebee08ae3266366f0a7c6e156e9ff
Karlp-Web <karlenportillo1@gmail.com>
2025-02-04 18:37:52 +0000

    yomomma

--- Main.java
+++ Main.java
@@ -35,13 +35,14 @@ public class Main
 
   public static boolean hasDuplicates(int[] arr)
   {
-    int count = 0;
-
     for (int i= 0; i < arr.length; i++)
     {
       if (arr[i] == target)
       {
-        count++
+        int target = arr[i];
+
+
+        
       }
     }
 
